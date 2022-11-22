a62afb3d2b3b1cf3de02febd8a481ea6e957752c
https://exzotic.co.uk <85530107+alieuk61@users.noreply.github.com>
2022-11-22 18:28:53 +0000

    Update styles.js

--- js/styles.js
+++ js/styles.js
@@ -1,37 +1,59 @@
-'use strict'
-
-import data from '../data/data.json' assert { type: 'json' } //this is how to get json data from another file
+import data from '../data/data.json' assert { type: 'json' }; //this is how to get json data from another file
 const barContainer = document.querySelector('.barchart-div')
 
 //creating the elements
 //we use of instead of in as json is an object in an array and we use of to loop in arrays
 for (let bar of data){ //literally just looping through the json object
   let day = document.createElement('h5');
-  let bar = document.createElement('div');
+
+  day.style.margin = `${10}px ${0}`;
+
+  let barElement = document.createElement('div');
   let barWrapper = document.createElement('div')
 
-  //giving the bar class names
+  //giving the bar names
   day.innerText = bar.day
   //give the bar(div) a size
-  bar.style.height = (bar.amount * 2.8) + 'px'
+  barElement.style.height = (bar.amount * 2.8) + 'px'
+  barElement.style.width = 50 + 'px'
   //giving bars colour, different colour if its wednesday
   if (bar.day == 'wed' ){
-    bar.style.backgroundColor = 'hsl(186, 34%, 60%);'
+    barElement.style.backgroundColor = 'hsl(186, 34%, 60%)'
   }else{
-    bar.style.backgroundColor = 'hsl(10, 79%, 65%);'
+    barElement.style.backgroundColor = 'hsl(10, 79%, 65%)'
   }
 
+  if (bar.day == 'wed' ){
+    barElement.classList.add('hover1')
+    barElement.classList.add("hover1:hover")
+  }else{
+    barElement.classList.add('hover')
+    barElement.classList.add("hover:hover")
+  }
+
+  barElement.style.borderRadius = 6 + 'px';
+  barWrapper.classList.add('bar-wrapper');
+
+  day.style.display = 'flex';
+  day.style.justifyContent = 'center';
+
+  let barAmount = document.createElement('div');
+  barAmount.style.height = 40 + 'px';
+  barAmount.style.width = '60px';
+  barAmount.style.backgroundColor = 'hsl(25, 47%, 15%)';
+  barAmount.innerText = '$' + bar.amount;
+  barAmount.style.display = 'flex';
+  barAmount.style.justifyContent = 'center';
+  barAmount.style.alignItems = 'center';
+  barAmount.style.display = 'none';
+
+  //create eventListener to make the barAmount appear and dissapear when hovered over
+
+
   //appendChild everything to container
+  barWrapper.appendChild(barAmount)
+  barWrapper.appendChild(barElement)
   barWrapper.appendChild(day)
-  barWrapper.appendChild(bar)
   barContainer.appendChild(barWrapper)
 
 }
-
-console.log(barContainer)
-
- //when hovering over a div we want the div to display data
-//we will make the div visible when that happens
-
-
- //div containing data (div.innerHTML (data.json data))
